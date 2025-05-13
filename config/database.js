1b2b35e89007980fabf536b5bb3a2baf586e9f67
LauS4nchez <130561569+LauS4nchez@users.noreply.github.com>
2025-05-13 16:14:33 -0300

    a

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
     connection: {
       connectionString: env('DATABASE_URL'), // Si usas la URL completa de Neon/Render
       host: env('DATABASE_HOST'), // Ej: dpg-12345678-a.oregon-postgres.render.com
-      port: env.int('DATABASE_PORT', 5432),
+      port: env.int('DATABASE_PORT', 1337),
       database: env('DATABASE_NAME'), // Nombre de tu DB en Render
       user: env('DATABASE_USERNAME'), // Usuario de la DB
       password: env('DATABASE_PASSWORD'), // Contraseña generada por Render
