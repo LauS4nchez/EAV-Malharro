ac15b39c902bcfc6020bd6e7c36e3d81a29ec6a3
LauS4nchez <130561569+LauS4nchez@users.noreply.github.com>
2025-05-19 09:45:39 -0300

    a

--- config/server.js
+++ config/server.js
@@ -4,11 +4,9 @@ module.exports = ({ env }) => ({
   app: {
     keys: env.array('APP_KEYS'),
   },
-  url: env('PUBLIC_URL', 'http://localhost:1337'), // Asegúrate de que esto esté configurado
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
-  // Agrega esto:
   settings: {
     allowedHosts: [
       'eav-malharro.onrender.com',
